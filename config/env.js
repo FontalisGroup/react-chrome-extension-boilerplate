303361c4997a30ec52af18e4fc197ffedb7fb619
ugrinovs <stefan.u@vivifyideas.com>
2018-11-26 09:42:35 +0100

    Remove unneeded config changes

--- config/env.js
+++ config/env.js
@@ -38,14 +38,6 @@ dotenvFiles.forEach(dotenvFile => {
   }
 });
 
-const NODE_PATH = process.env.NODE_PATH;
-
-if (!NODE_PATH) {
-  throw new Error(
-    'The NODE_PATH environment variable is required to enable absolute imports. Check if you have added it to .env file'
-  );
-}
-
 // We support resolving modules according to `NODE_PATH`.
 // This lets you use absolute paths in imports inside large monorepos:
 // https://github.com/facebook/create-react-app/issues/253.
@@ -62,23 +54,29 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
   .map(folder => path.resolve(appDirectory, folder))
   .join(path.delimiter);
 
+// Grab NODE_ENV and REACT_APP_* environment variables and prepare them to be
+// injected into the application via DefinePlugin in Webpack configuration.
+const REACT_APP = /^REACT_APP_/i;
+
 function getClientEnvironment(publicUrl) {
-  const raw = Object.keys(process.env).reduce(
-    (env, key) => {
-      env[key] = process.env[key];
-      return env;
-    },
-    {
-      // Useful for determining whether we’re running in production mode.
-      // Most importantly, it switches React into the correct mode.
-      NODE_ENV: process.env.NODE_ENV || 'development',
-      // Useful for resolving the correct path to static assets in `public`.
-      // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
-      // This should only be used as an escape hatch. Normally you would put
-      // images into the `src` and `import` them in code to get their paths.
-      PUBLIC_URL: publicUrl
-    }
-  );
+  const raw = Object.keys(process.env)
+    .filter(key => REACT_APP.test(key))
+    .reduce(
+      (env, key) => {
+        env[key] = process.env[key];
+        return env;
+      },
+      {
+        // Useful for determining whether we’re running in production mode.
+        // Most importantly, it switches React into the correct mode.
+        NODE_ENV: process.env.NODE_ENV || 'development',
+        // Useful for resolving the correct path to static assets in `public`.
+        // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
+        // This should only be used as an escape hatch. Normally you would put
+        // images into the `src` and `import` them in code to get their paths.
+        PUBLIC_URL: publicUrl
+      }
+    );
   // Stringify all values so we can feed into Webpack DefinePlugin
   const stringified = {
     'process.env': Object.keys(raw).reduce((env, key) => {
